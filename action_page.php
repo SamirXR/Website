d1a51f827c65c67be29aee04508683cda9f50612
Glitch (glitch-hello-website) <none>
2021-09-14 17:25:48 +0000

    🚍👑 Checkpoint ./index.html:22699063/9 ./action_page.php:22699063/22

--- action_page.php
+++ action_page.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["Name"]; ?><br>
-Your email address is: <?php echo $_POST["Email"]; ?>
+Welcome <?php echo $_GET["Name"]; ?><br>
+Your email address is: <?php echo $_GET["Email"]; ?>
 
 </body>
 </html>
