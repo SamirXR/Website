c05bbcd0cd923ad27465e2da40c27df299300408
Glitch (xr7-bots) <none>
2021-09-26 20:36:48 +0000

    📰👼 Checkpoint ./action_page.php:22699063/585

--- action_page.php
+++ action_page.php
@@ -1,8 +1,19 @@
-<html>
-<body>
+<?php
+error_reporting(-1);
+var_dump($_POST);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
 
-Welcome <?php echo var_dump($_POST); ["Name"]; ?><br>
-Your email address is: <?php echo $_POST ["Email"]; ?>
+    Welcome <?php echo $_POST["Name"]; ?><br>
+    Your email address is: <?php echo $_POST["Email"]; ?>
 
-</body>
+    </body>
 </html>
